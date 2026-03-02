9b2df102b49e46206bfb83c0e9ca0020d9238356
Mohamed Elgaar <mohdelgaar@gmail.com>
2026-03-02 14:11:02 -0500

    update configs

--- configs/gpus_4.sh
+++ configs/gpus_4.sh
@@ -4,7 +4,7 @@
 export NUM_GPUS=4
 
 # Number of learner processes per node
-export NUM_LEARNERS_PER_NODE=2
+export NUM_LEARNERS_PER_NODE=3
 
 # Number of vLLM inference engines
-export VLLM_NUM_ENGINES=2
+export VLLM_NUM_ENGINES=1
--- configs/gpus_5.sh
+++ configs/gpus_5.sh
@@ -4,7 +4,7 @@
 export NUM_GPUS=5
 
 # Number of learner processes per node
-export NUM_LEARNERS_PER_NODE=3
+export NUM_LEARNERS_PER_NODE=4
 
 # Number of vLLM inference engines
-export VLLM_NUM_ENGINES=2
+export VLLM_NUM_ENGINES=1
