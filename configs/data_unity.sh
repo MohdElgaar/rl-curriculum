3a78e5b96f74aaa9b203cd72c850699da76b88f5
Mohamed Elgaar <mohdelgaar@gmail.com>
2026-03-07 20:24:12 +0000

    configs

--- configs/data_unity.sh
+++ configs/data_unity.sh
@@ -7,4 +7,4 @@
 export SCRATCH_ROOT="/scratch4/workspace/mohamed_elgaar_student_uml_edu-rl-curriculum"
 export HF_HOME="${SCRATCH_ROOT}/cache/huggingface"
 export DATASET_LOCAL_CACHE_DIR="${SCRATCH_ROOT}/data/open-instruct"
-export OUTPUT_DIR="${SCRATCH_ROOT}/outputs/${EXP_NAME}"
+export OUTPUT_DIR="${SCRATCH_ROOT}/outputs"
